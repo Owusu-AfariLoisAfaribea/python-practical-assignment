e0b5ca4c6b2718d4a9610808cfb7f387049f3f12
Owusu-AfariLoisAfaribea <loisafari07@gmail.com>
2026-07-21 00:01:01 +0000

    Add files via upload

---
+++ my_introduction.py
@@ -0,0 +1,11 @@
+# i am creating my introductory python file.
+# i am uaing the hashtags here to show how comments are written.
+# this file is going to show my personal information.
+
+print("Full Name: Lois")
+print("Programme: Bsc. Information Technology")
+print("Level: Level 100")
+print("Hometown: Aburi")
+print("Favourite Food: Waakye")
+print("Why I Joined Python for Problem Solving: To learn programming and solve real-world problems using Python.")
+print("Dream Career: Cybersecurity Analyst and Network Security Administrator")
